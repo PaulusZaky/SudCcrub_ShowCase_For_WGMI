97c38f3ac13fceb7f911713c6e7783a7e7537375
PaulusZaky <pauluszaky@gmail.com>
2024-08-31 04:55:09 -0400

    fix: banner height & card margin

--- assets/custom.js
+++ assets/custom.js
@@ -1,11 +1,15 @@
 $('.mobile-collection .text-with-icon button').click(function () {
-    $('.mobile-collection .collection__facets').slideToggle(300);
-    $('.mobile-collection .text-with-icon button').toggleClass('opened');
-    if ($('.mobile-collection .text-with-icon button').hasClass('opened')) {
-      $('.mobile-collection>.text-with-icon').css('borderBottom', 'none');
-    } else {
-      setTimeout(() => {
-        $('.mobile-collection >.text-with-icon').css('border-bottom', '1px solid rgba(0, 0, 0, 0.1)');
-      }, 300);
-    }
-  });
+  $('.mobile-collection .collection__facets').slideToggle(300);
+  $('.mobile-collection .text-with-icon button').toggleClass('opened');
+  if ($('.mobile-collection .text-with-icon button').hasClass('opened')) {
+    $('.mobile-collection>.text-with-icon').css('borderBottom', 'none');
+  } else {
+    setTimeout(() => {
+      $('.mobile-collection >.text-with-icon').css('border-bottom', '1px solid rgba(0, 0, 0, 0.1)');
+    }, 300);
+  }
+});
+
+$('.facets__floating-filter>button').click(function() {
+  $("#facets-drawer").find('.accordion').removeAttr('open');
+})
